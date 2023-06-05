a3c1cbe9ab0d5af912dab2d65ff2aee5f642fddb
Letizia <116899981+letizia-sorrentino@users.noreply.github.com>
2023-06-05 09:58:43 +0100

    added components files

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Loading from './components/Loading';
-import Data from './components/Data';
+import React, { Component } from "react";
+import axios from "axios";
+import Loading from "./components/Loading";
+import Simpsons from "./components/Simpsons";
+import "./App.css";
 
 class App extends Component {
-  state = {  } 
+  state = {};
 
-async componentDidMount() {
+  async componentDidMount() {
+    const { data } = await axios.get(
+      `https://thesimpsonsquoteapi.glitch.me/quotes?count=50`
+    );
+    this.setState({ simpsons: data });
+  }
 
-const { data } = await axios.get(`https://thesimpsonsquoteapi.glitch.me/quotes?count=5`);
-//console.log(data);
-this.setState({data});
+  render() {
+    const { simpsons } = this.state;
 
-}
+    if (!simpsons) return <Loading />;
 
-  render() { 
-    //conditional rendering - if there is nothing in the state, show loading
-    if (!this.state.data) 
-    return <Loading/> //<div>Loading...</div>
-    return <Data />           //<div>I have the data</div>;
-
-    //in alternative you can use a ternary
-    // return this.state.data ? "Data" : "loading";
+    return (
+      <>
+        <h1>Total no of liked chars #</h1>
+        <Simpsons simpsons={simpsons} />
+      </>
+    );
   }
 }
- 
+
 export default App;
--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
+// import React, { Component } from "react";
 
-class Data extends Component {
-    render() { 
-        return (<div>I have the data</div>);
-    }
-}
- 
-export default Data;
+// class Data extends Component {
+//   render() {
+//     return (
+//       <> 
+//       <ol>
+//         {this.props.data.map((item) => {
+//           return <li key={item.quote}>{item.character}</li>;
+//         })}
+//       </ol>
+//       </>
+//     );
+
+//   }
+// }
+
+// export default Data;
